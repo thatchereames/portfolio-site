cf97533dbcb60f08dfe03b0945b7187b7a793dc2
Thatcher Eames <thatcher.eames@comcast.net>
2026-03-27 14:21:57 -0600

    Added a skeleton Welcome Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "welcome_page/welcome"
+  root 'welcome_page#welcome'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
